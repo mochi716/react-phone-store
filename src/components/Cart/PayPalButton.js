e6872eac9fd3055f836054be40bf984470c8d182
new_mochih <fam.mochih321@gmail.com>
2020-04-09 17:50:36 +0700

    first commit

---
+++ src/components/Cart/PayPalButton.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PaypalExpressBtn from 'react-paypal-express-checkout';
+ 
+export default class MyApp extends React.Component {
+    render() {
+        const onSuccess = (payment) => {
+            this.props.clearCart();
+            this.props.history.push('/');
+        }
+ 
+        const onCancel = (data) => {
+            console.log('The payment was cancelled!', data);
+        }
+ 
+        const onError = (err) => {
+            console.log("Error!", err);
+        }
+ 
+        let env = 'sandbox'; // you can set here to 'production' for production
+        let currency = 'USD'; // or you can set this value from your props or state
+        let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
+ 
+        const client = {
+            sandbox: process.env.REACT_APP_APP_ID,
+            production: 'YOUR-PRODUCTION-APP-ID',
+        }
+        return (
+            <PaypalExpressBtn env={env} client={client} currency={currency} total={this.props.total} onError={onError} onSuccess={onSuccess} onCancel={onCancel} />
+        );
+    }
+}
